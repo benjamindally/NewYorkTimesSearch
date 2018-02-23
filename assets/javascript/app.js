e39f10b2e21d43750184291e6f4211bde4335988
jaseat <jaseat@users.noreply.github.com>
2018-02-23 00:03:17 -0800

    finished displaying articles

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,34 +12,31 @@ var numberOfRecords;
 //https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=OURAPIKEY&q=SEARCHTERM
 
 function update(){
+    $("#container").empty();
     console.log(numberOfRecords);
     var imglink = "https://www.nytimes.com/"
-    for(var i = 0; i < numberOfRecords; i++){
-        //if the document isn't an article skip over it
-        if(res.docs[i].document_type !== "article"){
-            numberOfRecords++;
-            console.log(i+"success");
-        }
-        else{    
-            console.log(i+"failure");
-            console.log(res);
-            var article = $("<div>");
-            var headline = $("<h1>");
-            var image = $("<img>");
-            var snippet = $("<p>");
-            var link = $("<a>");
-            headline.html(res.docs[i].headline.main);
+    for(var i = 0; i < numberOfRecords; i++){   
+        var article = $("<div>");
+        var headline = $("<h1>");
+        var image = $("<img>");
+        var snippet = $("<p>");
+        var link = $("<a>");
+        headline.html(res.docs[i].headline.main);
+        if(res.docs[i].multimedia[0] !== undefined){
             var completeLink = imglink + res.docs[i].multimedia[0].url;
             image.attr("src", completeLink);
-            snippet.html(res.docs[i].snippet);
-            link.attr("href", res.docs[i].web_url);
-            link.html("Read more");
-            article.append(headline);
-            article.append(image);
-            article.append(snippet);
-            article.append(link);
-            $("#container").append(article);
         }
+        else{
+            image.attr("alt", "[no image found!]");
+        }
+        snippet.html(res.docs[i].snippet);
+        link.attr("href", res.docs[i].web_url);
+        link.html("Read more");
+        article.append(headline);
+        article.append(image);
+        article.append(snippet);
+        article.append(link);
+        $("#container").append(article);
     }
 }
 
@@ -52,7 +49,8 @@ $(document).ready(function(){
         console.log(query);
         console.log(numberOfRecords);
             $.ajax({
-                url: "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key="+APIKey+"&q="+query,
+                url: "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key="+APIKey+"&q="+query
+                    + "&fq=document_type:(\"article\")", //We want articles only
                 method: "GET"
             }).then(function(response){
                 //do
@@ -60,6 +58,11 @@ $(document).ready(function(){
                 console.log(res);
                 update();
             })
+    })
+
+    $("#clearBtn").on("click", event=>{
+        event.preventDefault();
+        $("#container").empty();
     })
 
 
