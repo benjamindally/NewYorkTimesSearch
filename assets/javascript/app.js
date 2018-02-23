72d122bc05c08d4777936de6b927115c0026360e
jaseat <jaseat@users.noreply.github.com>
2018-02-22 21:22:29 -0800

    ajax

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 var APIKey = "89e3ef8298d348709f256379ebdca622";
 var query = "test";
+var res;
 //store response in variable res
 //res.docs is array of found articles
 //res.docs[i].web_url is web url
@@ -9,14 +10,24 @@ var query = "test";
 // request looks like
 //https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=OURAPIKEY&q=SEARCHTERM
 
+function update(){
+
+}
+
 $(document).ready(function(){
 
-	$.ajax({
-		url: "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key="+APIKey+"&q="+query,
-		method: "GET"
-	}).then(function(response){
-		console.log(response);
-	})
+    $("#searchBtn").on("click", function(event){
+        event.preventDefault()
+        query = $("#searchTerm").val()
+            $.ajax({
+                url: "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key="+APIKey+"&q="+query,
+                method: "GET"
+            }).then(function(response){
+                //do
+                res = response;
+                update();
+            })
+    })
 
 
 
