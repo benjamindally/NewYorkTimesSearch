5d95907d5a27d1bd9aeeea2b69494841be77ed20
jaseat <jaseat@users.noreply.github.com>
2018-02-22 20:31:11 -0800

    added comments

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+var APIKey = "89e3ef8298d348709f256379ebdca622";
+
+//store response in variable res
+//res.docs is array of found articles
+//res.docs[i].web_url is web url
+//res.docs[i].headline.main is headline
+//res.docs[i].snippet is a snippet of the article
+
+// request looks like
+//https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=OURAPIKEY&q=SEARCHTERM
